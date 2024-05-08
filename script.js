679b517ac92003c83fee7d9677f7e89e81040596
Ashutosh Verma <vashutosh625@gmail.com>
2024-05-09 02:32:45 +0530

    Completed

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 const locomotiveScroll = new LocomotiveScroll();
 gsap.registerPlugin(ScrollTrigger);
 
-function startLoader() {
+const startLoader = () => {
   let counterElement = document.querySelector(".counter");
   let currentValue = 0;
-
   function updateCounter() {
     if (currentValue === 100) {
       return;
     }
-
     currentValue += Math.floor(Math.random() * 10) + 1;
-
     if (currentValue > 100) {
       currentValue = 100;
     }
-
     counterElement.textContent = currentValue + "%";
-
     let delay = Math.floor(Math.random() * 200) + 5;
     setTimeout(updateCounter, delay);
   }
-
   updateCounter();
-}
-
-function pageRevealAnimation() {
+};
+const cursor = () => {
+  const cursorDot = document.querySelector(".cursor-dot");
+  window.addEventListener("mousemove", function (e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+    cursorDot.animate(
+      {
+        left: `${posX}px`,
+        top: `${posY}px`,
+      },
+      { duration: 500, fill: "forwards" }
+    );
+  });
+};
+const pageRevealAnimation = () => {
   const pageRevealTimeline = gsap.timeline();
-  const target = document.querySelectorAll(".heading-text h1");
-  Splitting({ target: target[0], whitespace: true })[0].chars;
-  Splitting({ target: target[1], whitespace: true })[0].chars;
+  SplitType.create(".heading-text h1");
   pageRevealTimeline
     .to("#preloader p, #preloader h2", {
       y: -300,
@@ -50,7 +55,7 @@ function pageRevealAnimation() {
       ">-1.4"
     )
     .to(
-      ".char",
+      ".heading-text h1 .char",
       {
         y: "0",
         duration: 1.1,
@@ -60,7 +65,7 @@ function pageRevealAnimation() {
       ">-1.3"
     )
     .from(
-      ".circle3",
+      ".home-circle",
       {
         scale: 0,
         opacity: 0,
@@ -78,27 +83,34 @@ function pageRevealAnimation() {
         ease: "power2.out",
       },
       ">-.8"
+    )
+    .from(
+      "#hero-text h5",
+      {
+        y: "20%",
+        opacity: 0,
+        duration: 1.1,
+        ease: "power2.out",
+      },
+      ">-1"
     );
-}
-
+};
 function scrollToAnimations() {
   const homeButton = document.getElementsByClassName("scrollTo-home");
   const aboutButton = document.getElementsByClassName("scrollTo-about");
   const skillButton = document.getElementsByClassName("scrollTo-skill");
   const projectButton = document.getElementsByClassName("scrollTo-projects");
   const contactButton = document.getElementsByClassName("scrollTo-contact");
-
   const $homePage = document.getElementById("home-page");
   const $aboutPage = document.getElementById("about-page");
   const $skillPage = document.getElementById("skill-page");
   const $projectPage = document.getElementById("project-page");
-  const $contactPage = document.getElementById("contact-page");
+  const $contactPage = document.getElementById("transparent-page");
 
   function scrollTo(params) {
     const { target, options } = params;
     locomotiveScroll.scrollTo(target, options);
   }
-
   homeButton[0].addEventListener("click", function () {
     scrollTo({
       target: $homePage,
@@ -119,7 +131,6 @@ function scrollToAnimations() {
       },
     });
   });
-
   for (let index = 0; index < 2; index++) {
     aboutButton[index].addEventListener("click", function () {
       scrollTo({
@@ -131,7 +142,6 @@ function scrollToAnimations() {
         },
       });
     });
-
     skillButton[index].addEventListener("click", function () {
       scrollTo({
         target: $skillPage,
@@ -160,33 +170,125 @@ const logoMoveAnimation = () => {
     let position = logo.getBoundingClientRect();
     let x = e.pageX - position.left - position.width / 2;
     let y = e.pageY - position.top - position.height / 2;
-
     logo.style.transform = `translateX(${x * 0.4}px) translateY(${y * 0.4}px)`;
   });
   logo.addEventListener("mouseleave", (e) => {
     logo.style.transform = "translate(0px, 0px)";
   });
 };
-
-const workPageAnimations = () => {
+const aboutPageAnimations = () => {
   const aboutHeading = document.querySelector(".about-heading");
-  Splitting({ target: aboutHeading, whitespace: true })[0].chars;
+  SplitType.create(".about-heading");
+  SplitType.create("#about-content-right p");
   gsap.to(".about-heading .char", {
-    delay: .5,
+    delay: 0.3,
     y: 0,
-    duration: 1,
+    duration: 0.7,
     ease: "power3.inOut",
     stagger: 0.04,
     scrollTrigger: {
-        trigger: '#about-page',
-        markers: true,
-        start: "top 80%",
-    }
+      trigger: "#about-page",
+      start: "top 80%",
+    },
+  });
+  gsap.to("#about-content-right p .line .word", {
+    delay: 0.5,
+    y: 0,
+    duration: 0.6,
+    ease: "power3.inOut",
+    stagger: 0.01,
+    scrollTrigger: {
+      trigger: "#about-page",
+      start: "top 80%",
+    },
+  });
+};
+const skillPageAnimation = () => {
+  SplitType.create(".skill-heading");
+  SplitType.create("#skill-content p");
+  gsap.to(".skill-heading .char", {
+    delay: 0.3,
+    y: 0,
+    duration: 0.7,
+    ease: "power3.inOut",
+    stagger: 0.04,
+    scrollTrigger: {
+      trigger: "#skill-page",
+      start: "top 90%",
+    },
+  });
+  gsap.to("#skill-content p .line .word", {
+    delay: 0.1,
+    y: 0,
+    duration: 0.6,
+    ease: "power3.inOut",
+    stagger: 0.01,
+    scrollTrigger: {
+      trigger: "#skill-content p",
+      start: "top 97%",
+    },
+  });
+};
+const projectPageAnimation = () => {
+  SplitType.create(".project-heading");
+
+  gsap.to(".project-heading .char", {
+    delay: 0.3,
+    y: 0,
+    duration: 0.7,
+    ease: "power3.inOut",
+    stagger: 0.04,
+    scrollTrigger: {
+      trigger: "#project-page",
+      start: "top 90%",
+    },
+  });
+};
+const contactPageAnimation = () => {
+  SplitType.create("#contact-page h1");
+
+  gsap.to("#contact-page h1 .char", {
+    delay: 0.3,
+    y: 0,
+    duration: 0.7,
+    ease: "power3.inOut",
+    stagger: 0.08,
+    scrollTrigger: {
+      trigger: "#project-page",
+      start: "bottom 60%",
+    },
+  });
+
+  gsap.from(".contact-circle", {
+    opacity: 0,
+    scale: 0,
+    ease: "back",
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#project-page",
+      start: "bottom 50%",
+    },
+  });
+
+  gsap.from("footer", {
+    delay: 1,
+    y: "40%",
+    opacity: 0,
+    ease: "power1.out",
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: "#project-page",
+      start: "bottom 40%",
+    },
   });
 };
 
+cursor();
 startLoader();
 pageRevealAnimation();
 scrollToAnimations();
 logoMoveAnimation();
-workPageAnimations();
+aboutPageAnimations();
+skillPageAnimation();
+projectPageAnimation();
+contactPageAnimation();
